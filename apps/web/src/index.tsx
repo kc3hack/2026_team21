12dc13152b458c6c679331df08156a8e4d7674ce
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2026-02-18 23:53:27 +0900

    ページを ./pages に移動する

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,12 +1,3 @@
-import { Hono } from "hono";
-import { renderer } from "./renderer";
-
-const app = new Hono();
-
-app.use(renderer);
-
-app.get("/", (c) => {
-  return c.render(<h1>Hello!</h1>);
-});
+import app from "@/pages";
 
 export default app;
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,17 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 import ssrPlugin from "vite-ssr-components/plugin";
 
+const dirname = typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [cloudflare(), ssrPlugin()],
+  base: "./",
+  resolve: {
+    alias: {
+      "@": path.resolve(dirname, "./src"),
+    },
+  },
 });
