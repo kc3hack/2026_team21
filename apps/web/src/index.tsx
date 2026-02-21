e3ea891877fb23dce66339d82133a2d7e052c4d8
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2026-02-22 06:45:35 +0900

    Routing をまとめる

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,14 +1,4 @@
-import { Hono } from "hono";
-import type { WorkerEnv } from "@/app";
-import pages from "@/pages";
-import { AnimationTestPage } from "@/pages/test";
-import api from "@/routes/api";
-
-const app = new Hono<WorkerEnv>();
-
-app.route("/api", api);
-app.route("/test", AnimationTestPage);
-app.route("/", pages);
+import app from "@/pages";
 
 export { DoorMan } from "@/durable-objects/DoorMan";
 export default app;
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,17 +1,21 @@
 import { Hono } from "hono";
+import api from "@/pages/api";
 import { CounterPageRoute } from "@/pages/counter";
 import { DebugPage } from "@/pages/debug";
+import { TopPageRoute } from "@/pages/index.page";
 import { RoomPageRoute } from "@/pages/r";
 import { renderer } from "@/pages/renderer";
-import { Page } from "@/pages/router";
+import { AnimationTestPage } from "@/pages/test";
 
 const app = new Hono();
 
 app.use(renderer);
 
-app.get("/", (c) => c.render(<Page id="/" />));
+app.route("/", TopPageRoute());
+app.route("/api", api);
 app.route("/r", RoomPageRoute());
 app.route("/debug", DebugPage);
 app.route("/counter", CounterPageRoute);
+app.route("/test", AnimationTestPage);
 
 export default app;
