eddb1b9b99c4093d5387718e7ea7cc39dbf85507
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2026-02-21 23:17:03 +0900

    試験的につけていたオプションを消す

--- apps/web/src/lib/qrcode/index.ts
+++ apps/web/src/lib/qrcode/index.ts
@@ -40,65 +40,13 @@ export const createQRCode = (text: string) => {
       color: "#ffffff",
     },
     image: undefined,
-    // dotsOptionsHelper: {
-    //   colorType: {
-    //     single: true,
-    //     gradient: false,
-    //   },
-    //   gradient: {
-    //     linear: true,
-    //     radial: false,
-    //     color1: "#6a1a4c",
-    //     color2: "#6a1a4c",
-    //     rotation: "0",
-    //   },
-    // },
     cornersSquareOptions: {
       type: "extra-rounded",
       color: "#000000",
     },
-    // cornersSquareOptionsHelper: {
-    //   colorType: {
-    //     single: true,
-    //     gradient: false,
-    //   },
-    //   gradient: {
-    //     linear: true,
-    //     radial: false,
-    //     color1: "#000000",
-    //     color2: "#000000",
-    //     rotation: "0",
-    //   },
-    // },
     cornersDotOptions: {
       type: "extra-rounded",
       color: "#000000",
     },
-    // cornersDotOptionsHelper: {
-    //   colorType: {
-    //     single: true,
-    //     gradient: false,
-    //   },
-    //   gradient: {
-    //     linear: true,
-    //     radial: false,
-    //     color1: "#000000",
-    //     color2: "#000000",
-    //     rotation: "0",
-    //   },
-    // },
-    // backgroundOptionsHelper: {
-    //   colorType: {
-    //     single: true,
-    //     gradient: false,
-    //   },
-    //   gradient: {
-    //     linear: true,
-    //     radial: false,
-    //     color1: "#ffffff",
-    //     color2: "#ffffff",
-    //     rotation: "0",
-    //   },
-    // },
   });
 };
