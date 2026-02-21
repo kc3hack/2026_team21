38de690f3feb4249043f92649a2885096a4f5927
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2026-02-21 22:38:19 +0900

    lint/performance/noImgElement の  suppress を消す

--- apps/web/src/pages/HomeClient.tsx
+++ apps/web/src/pages/HomeClient.tsx
@@ -2,7 +2,6 @@ export const HomeClient = () => {
   return (
     <main class="home-container">
       <div class="logo-area">
-        {/* biome-ignore lint/performance/noImgElement: Hono does not use Next.js Image */}
         <img src="/images/logo.PNG" alt="Gost" class="logo-img" />
       </div>
 
@@ -12,22 +11,16 @@ export const HomeClient = () => {
           <div class="ghost-body" id="ghost-character">
             <div class="exclamation-mark">!</div>
 
-            {/* biome-ignore lint/performance/noImgElement: Hono does not use Next.js Image */}
             <img src="/images/home/body.svg" alt="body" class="ghost-part" />
-            {/* biome-ignore lint/performance/noImgElement: Hono does not use Next.js Image */}
             <img src="/images/home/head.svg" alt="head" class="ghost-part" />
 
-            {/* biome-ignore lint/performance/noImgElement: Hono does not use Next.js Image */}
             <img id="ghost-arm-right" src="/images/home/right.svg" alt="right" class="ghost-part" />
-            {/* biome-ignore lint/performance/noImgElement: Hono does not use Next.js Image */}
             <img id="ghost-arm-left" src="/images/home/left.svg" alt="left" class="ghost-part" />
 
             <div class="cheek cheek-left" />
             <div class="cheek cheek-right" />
 
-            {/* biome-ignore lint/performance/noImgElement: Hono does not use Next.js Image */}
             <img id="ghost-eye-right" src="/images/home/right_eye.svg" alt="right_eye" class="ghost-part ghost-eye" />
-            {/* biome-ignore lint/performance/noImgElement: Hono does not use Next.js Image */}
             <img id="ghost-eye-left" src="/images/home/left_eye.svg" alt="left_eye" class="ghost-part ghost-eye" />
           </div>
           <div class="ghost-shadow" />
