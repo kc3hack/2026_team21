2975f90362f51e6b1d958f66e81e4821e42f4c91
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2026-02-22 03:35:26 +0900

    仮ページを作る

--- apps/web/src/lib/client/helper.tsx
+++ apps/web/src/lib/client/helper.tsx
@@ -1,17 +1,3 @@
-import type { ComponentId } from "@/pages/router";
-
-/**
- * Client Componentをマウントするためのヘルパーコンポーネント
- *
- * @example
- * // 指定したIDにClient Componentがマウントされる
- * <ClientMount id="my-component" />
- *
- */
-export function ClientMount({ id }: { id: ComponentId }) {
-  return <div id={id} />;
-}
-
 /**
  * Client Componentをレンダリングするためのスクリプトタグ
  * グローバルに1度だけ読み込めば良い
